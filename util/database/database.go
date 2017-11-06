c2a33d4321c49495eb46238bae6d5832067595ee
0c34 <sulhaedir05@gmail.com>
2017-11-06 21:43:48 +0700

    modifie connect function to create database if not exist

--- util/database/database.go
+++ util/database/database.go
@@ -9,12 +9,33 @@ import(
 )
 
 func Connect()(*sql.DB, error){
+
 	config := config.LoadConfig()
-	cred := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", config.User,config.Password,config.Sqlhost,config.Sqlport,config.Dbname)
-	db, err := sql.Open("mysql",cred)
+
+	var dsn string
+	var db *sql.DB
+
+	dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/", config.User,config.Password,config.Sqlhost,config.Sqlport)
+	db, err := sql.Open("mysql",dsn)
+
 	if err != nil{
 		return nil,err
 	}
+	_,err = db.Exec("CREATE DATABASE IF NOT EXISTS "+config.Dbname)
+
+	if err != nil{
+		return nil,err
+	}else{
+
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", config.User,config.Password,config.Sqlhost,config.Sqlport,config.Dbname)
+		db, err = sql.Open("mysql",dsn)
+
+		if err != nil{
+			return nil,err
+
+		}
+	}
+
 	return db,nil
 }
 
@@ -22,13 +43,17 @@ var DB *sql.DB
 func CheckDatabase()(bool, error){
 
 	/* this function use to check if no database selected and will redirect to setup page */
+	
 	DB, err := Connect()
 	if err != nil{
 		log.Printf("Connection Error %s ",err.Error())
 	}
 
-	sql := "USE govwa"
-	result, err := DB.Exec(sql)
+	const (
+		checksql = `SELECT 1  FROM Users limit 1` //this will check if Table dbname.Users exist otherways will redirect to setup page 
+	)
+	result, err := DB.Exec(checksql)
+
 	if err != nil{
 		log.Println(err.Error())
 		return false,err
