55a3e1175324ee4d7fd56203594665a7d3a9273c
0c34 <sulhaedir05@gmail.com>
2017-11-06 21:42:39 +0700

    fixing all setup handler function

--- setup/setup.go
+++ setup/setup.go
@@ -5,6 +5,7 @@ import(
 	"net/http"
 
 	"govwa/util"
+	"govwa/util/config"
 	"govwa/util/database"
 	"govwa/util/middleware"
 
@@ -26,6 +27,12 @@ func (self Setup) SetRouter(r *httprouter.Router) {
 
 }
 
+type JsonResp struct{
+	Status string `json:"status"`
+	Message string `json:"message"`
+}
+
+
 func setupViewHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
 
 	var info string
@@ -34,18 +41,56 @@ func setupViewHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Param
 	ok, err := database.CheckDatabase()
 	
 	if !ok || err != nil{
-		info = fmt.Sprintf(`<div id="info" class="alert alert-danger">%s</div>`,err.Error())
+		info = fmt.Sprintf(`<div id="info" class="alert alert-danger">%s
+							<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></div>`,err.Error())
+		data["error"] = util.ToHTML(info)
+	}else{
+
+		info = fmt.Sprintf(`<div id="info" class="alert alert-success">Connection Success Click Reset to reset Database<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></div>`)
 		data["error"] = util.ToHTML(info)
 	}
 
-	info = fmt.Sprintf(`<div id="info" class="alert alert-success">Connection Success</div>`)
-	data["error"] = util.ToHTML(info)
-
 	data["title"] = "Setup/Reset"
-
+	data["weburl"] = config.Fullurl
 	util.SafeRender(w,r,"template.setup", data)
 }
 
+
 func setupActionHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
 	
+	if r.Method == "POST" && r.FormValue("act") == "cr"{
+		
+		res := []JsonResp{}
+		loginUrl := util.ToHTML(fmt.Sprintf(`<a href=%slogin>Login</a>`,config.Fullurl))
+		
+		err = createUsersTable() //create users table
+		if err != nil{
+			res = append(res, JsonResp{
+				Status : "0",
+				Message : err.Error(),
+			})
+		}else{
+			res = append(res, JsonResp{
+				Status : "1",
+				Message : "Create Users Table Success Please "+string(loginUrl),
+			})
+		}
+		
+		err = createProfileTable() //create profilet table
+		
+		if err != nil{
+			res = append(res, JsonResp{
+				Status : "0",
+				Message : err.Error(),
+			})
+		}else{
+			res = append(res, JsonResp{
+				Status : "1",
+				Message : "Create Profile Table Success Please "+string(loginUrl),
+			})
+		}
+
+		util.RenderAsJson(w, res)
+
+	}
 }
